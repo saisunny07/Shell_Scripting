05da108c8b1225d2ebe996c41ae4d67bf9b19278
new-at-654654561453 <kothurisaisunny5@gmail.com>
2024-08-04 20:05:48 -0500

    Updated example folder

--- examples/number.sh
+++ examples/number.sh
@@ -13,3 +13,8 @@ x="sai sunny"
 grep -o "s" <<< "$x" | wc -l
 
 
+# $x ==> variable 'x'
+# -o ==> only
+# Here grep selects only letter 's' from the variable x
+# wc ==> word count, 'wc -l' counts number of lines
+
